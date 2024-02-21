6aeb33fcaa8fb1dc06f9ebf2819c221719c9cbd1
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-21 12:48:18 +0600

    added react query mutatuion and added devtools in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const POSTS = [
   { id: 1, name: "ashik" },
@@ -6,15 +6,29 @@ const POSTS = [
 ];
 
 const App = () => {
+  const queryClient = useQueryClient();
+
   const postsQuery = useQuery({
-    queryKey: "posts",
+    queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const postsQueryMutation = useMutation({
+    mutationFn: async (title) => {
+      const newPost = { id: Date.now(), name: title }; // Create a new post object
+      POSTS.push(newPost); // Push the new post to the POSTS array
+      await wait(1000); // Wait for 1 second
+      return newPost; // Return the new post
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]); // Invalidate the cache to trigger a refetch
+    },
   });
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
 
   if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
-
+  console.log(POSTS);
   return (
     <div className="text-center ">
       <h1 className="font-bold text-2xl">Tanstack query App</h1>
@@ -23,6 +37,13 @@ const App = () => {
           <article key={post.id}>{post.name}</article>
         ))}
       </p>
+      <button
+        disabled={postsQueryMutation.isLoading}
+        className="bg-blue-500 py-2 px-4 text-white"
+        onClick={() => postsQueryMutation.mutate("hello")}
+      >
+        Add{" "}
+      </button>
     </div>
   );
 };
